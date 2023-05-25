559ba407c7f527b542bad1b052dafe29307d9fbb
williamstrauch11 <william.strauch11@gmail.com>
2023-05-25 11:42:11 -0500

    Add first script

---
+++ Traffic_V2_Unity/Assets/Scripts/RunSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class RunSettings
+{
+
+    // Lane
+    public static int LaneNum { get; } = 2;
+
+    //Car Number
+    public static int CarNum { get; } = 15;
+
+}
